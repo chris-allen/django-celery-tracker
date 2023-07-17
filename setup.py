d7ee8190b944bac8d73431597423139fdefd3fe3
Chris Allen <chris@apaxsoftware.com>
2023-07-17 12:05:27 -0400

    Added workflow for publishing releases to PyPI

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ classes = """
     Programming Language :: Python :: Implementation :: PyPy
     Framework :: Django
     Framework :: Django :: 3.2
-    Framework :: Django :: 4.0
+    Framework :: Django :: 4.2
     Operating System :: OS Independent
     Topic :: Communications
     Topic :: System :: Distributed Computing
